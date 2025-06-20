fb3a7e3560daf82aa36bc1418230beee32205623
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-06-20 20:04:28 +0900

    feat : 프로젝트 초기 셋팅

---
+++ Assets/@Scripts/Utils/Define.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define : MonoBehaviour
+{
+
+    /*
+     *  전역적 static 변수 
+     *  ex) public static float POTION_COLLECT_DISTANCE = 2.6F;
+     *  
+     *    public enum EState
+     *    {
+     *          NONE,
+     *    }
+     */
+
+
+
+}
