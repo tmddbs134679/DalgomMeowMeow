837ef85bb9eae042e49157b77c0fe047db0e26b3
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-06-23 08:49:25 +0900

    feat : 1. Newtonsoft.Json Package 추가 2. ResourceManager, DataManager 스크립트 추가

---
+++ Assets/@Scripts/Managers/Core/DataManager.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILoader<key, Value>
+{
+    Dictionary<key, Value> MakeDict();
+}
+
+public class DataManager 
+{
+    public Dictionary<int, Data.CreatureData> CreatureDic { get; private set; } = new Dictionary<int, Data.CreatureData>();
+
+
+    public void Init()
+    {
+        CreatureDic = LoadJson<Data.CreatureDataLoader, int, Data.CreatureData>("CreatureData").MakeDict();
+    }
+
+
+
+    Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
+    {
+        TextAsset textAsset = Managers.Resource.Load<TextAsset>($"{path}");
+        return JsonConvert.DeserializeObject<Loader>(textAsset.text);
+    }
+}
