b4594e7d38902508f5b820be5719bc2e2d11f1e7
Sungju Kim <tjdwn0885@naver.com>
2025-06-23 10:06:49 +0900

    feat: 데이터 제작
    감자, 당근, 양파, 양배추

---
+++ Assets/@Scripts/Data/Ingredient/CS/IngredientData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "IngredientData", menuName = "ScriptableObjects/IngredientData")]
+
+public class IngredientData : ScriptableObject
+{
+    public int ItemID;
+    public string Name;
+    public string Description;
+    public Sprite Icon;
+    public int Price;
+}
