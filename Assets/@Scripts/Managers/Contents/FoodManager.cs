0b5dd142e08de46b7f3eda51e6b58f35916000e5
Sungju Kim <tjdwn0885@naver.com>
2025-06-24 15:48:50 +0900

    Feat: 푸드매니저에서 UI로 할당기능 추가
    푸드매니저에서 UI로 아이콘을 할당할 수 있습니다, Fooddata 간단히 생성

--- Assets/@Scripts/Managers/Contents/FoodManager.cs
+++ Assets/@Scripts/Managers/Contents/FoodManager.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FoodManager : MonoBehaviour
 {
-    /*
-    public SpriteRenderer[] DisplaySprite;
-
+    
+    public GameObject[] ServeSlot;
+    private Image[] _serveSlotImages;
     private Queue<FoodData> _foodQueue = new Queue<FoodData>();
 
+    private void Awake()
+    {
+        SetComponent();
+    }
 
-
+    private void SetComponent()
+    {
+        _serveSlotImages = new Image[ServeSlot.Length];
+        for (int i = 0; i < ServeSlot.Length; i++)
+        {
+            _serveSlotImages[i] = ServeSlot[i].GetComponent<Image>();
+        }
+    }
 
     public void Enqueue(FoodData food)
     {
         _foodQueue.Enqueue(food);
+        SetSprite();
     }
 
     public FoodData Dequeue()
@@ -24,7 +36,9 @@ public class FoodManager : MonoBehaviour
             Debug.Log("Empty!");
             return null;
         }
-        return _foodQueue.Dequeue();
+        var food = _foodQueue.Dequeue();
+        SetSprite();
+        return food;
     }
 
     
@@ -33,17 +47,38 @@ public class FoodManager : MonoBehaviour
         int index = 0;
         foreach(var food in _foodQueue)
         {
-            if(index >= DisplaySprite.Length)
+            if(index >= ServeSlot.Length)
                 break;
 
-            DisplaySprite[index].sprite = food.sprite;
+            _serveSlotImages[index].sprite = food.Icon;
+            ServeSlot[index].SetActive(true);
             index++;
         }
+
+        for (; index < ServeSlot.Length; index++)
+        {
+            ServeSlot[index].SetActive(false);
+        }
     }
+    [ContextMenu("큐 확인")]
+    public void QueueCheck()
+    {
+        foreach (var food in _foodQueue)
+        {
+            Debug.Log(food.name);
+        }
+    }
+    public void SlotOff()
+    {
+        foreach (var slot in ServeSlot)
+        {
+            slot.SetActive(false);
+        }
+    }
+
     public void Clear()
     {
         _foodQueue.Clear();
+        SlotOff();
     }
-    */
-
 }
