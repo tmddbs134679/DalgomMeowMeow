72da79d61213e4ddb8b0bfc97e5f8b6367b75b99
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-07-01 15:29:18 +0900

    feat : 1. Equipment 클래스 추가 2. EquipmentData 엑셀 추가 및 데이터 연결

--- Assets/@Scripts/Managers/Core/DataManager.cs
+++ Assets/@Scripts/Managers/Core/DataManager.cs
@@ -13,12 +13,14 @@ public class DataManager
     public Dictionary<string, Data.CreatureData> CreatureDic { get; private set; } = new Dictionary<string, Data.CreatureData>();
     public Dictionary<string, Data.FoodData> FoodDic { get; private set; } = new Dictionary<string, Data.FoodData>();
 
+    public Dictionary<string, Data.EquipmentData> EquipmentDic { get; private set; } = new Dictionary<string, Data.EquipmentData>();
     public Dictionary<string, Data.BuildingData> BuildingDic { get; private set; } = new Dictionary<string, Data.BuildingData>();
     public void Init()
     {
         CreatureDic = LoadJson<Data.CreatureDataLoader, string, Data.CreatureData>("CreatureData").MakeDict();
         FoodDic = LoadJson<Data.FoodDataLoader, string, Data.FoodData>("FoodData").MakeDict();
         BuildingDic = LoadJson<Data.BuildingDataLoader, string, Data.BuildingData>("BuildingData").MakeDict();
+        EquipmentDic = LoadJson<Data.EquipmentDataLoader, string, Data.EquipmentData>("EquipmentData").MakeDict();
     }
 
 
