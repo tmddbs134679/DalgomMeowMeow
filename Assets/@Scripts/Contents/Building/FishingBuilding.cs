3bee8632272fbafd6f7c65a52741ee2bdb6df497
kihyun <gosof@naver.com>
2025-07-02 16:04:45 +0900

    feat: fishingbuilding 기능 추가

--- Assets/@Scripts/Contents/Building/FishingBuilding.cs
+++ Assets/@Scripts/Contents/Building/FishingBuilding.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum FishingResult
+{
+    Miss,       // 놓침
+    Normal,     // 평범
+    Jackpot     // 월척
+}
 public class FishingBuilding : BuildingBase
 {
     [SerializeField] private Renderer buildingRenderer;
 
     public GameObject collectIcon;
+    
+    [Header("낚시 확률 (0~1)")]
+    [Range(0f, 1f)] public float JackpotProbability = 0.1f;
+    [Range(0f, 1f)] public float NormalProbability = 0.7f;
     
     public override bool Init()
     {
@@ -16,14 +25,42 @@ public class FishingBuilding : BuildingBase
     }
     public override void Produce()
     {
-        Debug.Log("낚시 완료");
-        
-        StoredCount++; //  생산 누적
-        
-        Debug.Log($"낚시 완료! 누적 수량: {StoredCount}");
 
-        // collectIcon.SetActive(true);
+        FishingResult result = GetFishingResult();
 
+        switch (result)
+        {
+            case FishingResult.Jackpot:
+                Debug.Log("월척 희귀한 물고기 획득!");
+                StoredCount++; // 월척도 카운트에 포함
+                break;
+
+            case FishingResult.Normal:
+                Debug.Log(" 평범한 물고기 획득");
+                StoredCount++;
+                break;
+
+            case FishingResult.Miss:
+                Debug.Log(" 놓침");
+                break;
+        }
+
+        Debug.Log($"총 누적 수량: {StoredCount}");
+
+        // if (StoredCount > 0) collectIcon.SetActive(true);
+
+    }
+    
+    private FishingResult GetFishingResult()
+    {
+        float rand = Random.value; // 0.0 ~ 1.0
+
+        if (rand < JackpotProbability)
+            return FishingResult.Jackpot;
+        else if (rand < JackpotProbability + NormalProbability)
+            return FishingResult.Normal;
+        else
+            return FishingResult.Miss;
     }
     public void Collect()
     {
