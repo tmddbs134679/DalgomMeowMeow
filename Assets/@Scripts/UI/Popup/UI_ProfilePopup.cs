b1b7e3682fb5bfb03dc20abbc34db8271e4e71a6
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-07-03 01:19:48 +0900

    feat : 3D 프로필 이미지 구현중

--- Assets/@Scripts/UI/Popup/UI_ProfilePopup.cs
+++ Assets/@Scripts/UI/Popup/UI_ProfilePopup.cs
@@ -55,6 +55,9 @@ public class UI_ProfilePopup : UI_Popup
         GetButton((int)Buttons.PrevCharacterButton).gameObject.BindEvent(() => OnClickChangeButton(-1));
         GetButton((int)Buttons.NextCharacterButton).gameObject.BindEvent(() => OnClickChangeButton(1));
 
+        //GetRawImage((int)Images.Image).
+
+
         _equipHatItem = GetObject((int)GameObjects.EquippedIHattem).GetComponent<UI_EquipItem>();
         _equipAccessoryItem = GetObject((int)GameObjects.EquippedAccessoryItem).GetComponent<UI_EquipItem>();
         _equipBagItem = GetObject((int)GameObjects.EquippedBagItem).GetComponent<UI_EquipItem>();
@@ -69,13 +72,13 @@ public class UI_ProfilePopup : UI_Popup
     private void OnClickChangeButton(int dir)
     {
 
-        // Todo : ´ÙÀ½ Ä³¸¯ÅÍ, ÀÌÀü Ä³¸¯ÅÍ
+        // Todo : ë‹¤ìŒ ìºë¦­í„°, ì´ì „ ìºë¦­í„°
         List<Character> characterList = Managers.Game.Characters;
 
         if (characterList == null || characterList.Count == 0 || _character == null)
             return;
 
-        // Áßº¹ Ä³¸¯ÅÍ¸é °íÀ¯ id¸¦ »ç¿ëÇØ¾ßÇÏ³ª? »ı°¢
+        // ì¤‘ë³µ ìºë¦­í„°ë©´ ê³ ìœ  idë¥¼ ì‚¬ìš©í•´ì•¼í•˜ë‚˜? ìƒê°
         int currentIndex = characterList.FindIndex(c => c.Id == _character.Id);
         if (currentIndex == -1)
             return;
@@ -84,7 +87,7 @@ public class UI_ProfilePopup : UI_Popup
 
         _character = characterList[nextIndex];
 
-        // ÀÌÈÄ Ã³¸® (UI ¾÷µ¥ÀÌÆ® µî)
+        // ì´í›„ ì²˜ë¦¬ (UI ì—…ë°ì´íŠ¸ ë“±)
         SetInfo(_character);
 
     }
@@ -108,7 +111,7 @@ public class UI_ProfilePopup : UI_Popup
         //    item.SetInfo(quip);
         //}
 
-        //Todo : ¸ğÀÚ, °¡¹æ, ¾Ç¼¼¼­¸® ¾ÆÀÌÅÛ ³ª¿À¸é ¿¬°á MakeSubItem¸»°í °íÁ¤°ªµé
+        //Todo : ëª¨ì, ê°€ë°©, ì•…ì„¸ì„œë¦¬ ì•„ì´í…œ ë‚˜ì˜¤ë©´ ì—°ê²° MakeSubItemë§ê³  ê³ ì •ê°’ë“¤
 
         Refresh();
     }
@@ -120,5 +123,7 @@ public class UI_ProfilePopup : UI_Popup
         _equipHatItem.SetInfo(_character); 
         _equipAccessoryItem.SetInfo(_character);
         _equipBagItem.SetInfo(_character);
+
+        Managers.Object.Spawn<AICharacter>(new Vector3(500, 500, 500), _character.DataId);
     }
 }
