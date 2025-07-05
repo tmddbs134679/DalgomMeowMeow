ad0ec1464f005c0614fa6db2cba20a74b0c1ae78
Sungju Kim <tjdwn0885@naver.com>
2025-07-05 15:05:09 +0900

    fix: 버튼 리매칭
    버튼이 잘못 매칭된것 수정했습니다

--- Assets/@Scripts/UI/Popup/UI_PausePopup.cs
+++ Assets/@Scripts/UI/Popup/UI_PausePopup.cs
@@ -4,13 +4,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 using static Define;
 
-public class UI_PausePopup : UI_Popup
+public class UI_PausePopup : UI_Popup   //어드레서블에 프리펩 넣기
 {
     #region Enum
 
     enum Buttons
     {
-        ResumeButton,
+        ResumeButton,   //버튼이름(알아서 찾아옴, 게임오브젝트랑 이름 똑같이)
         RetryButton,
         TitleButton
     }
@@ -31,15 +31,33 @@ public class UI_PausePopup : UI_Popup
             return false;
 
         BindButton(typeof(Buttons));
-        /*
-        GetButton((int)Buttons.ResumeButton).gameObject.BindEvent(OnClickResumeButton);
+        
+        GetButton((int)Buttons.ResumeButton).gameObject.BindEvent(OnClickResumeButton); //버튼 등록
         GetButton((int)Buttons.RetryButton).gameObject.BindEvent(OnClickRetryButton);
         GetButton((int)Buttons.TitleButton).gameObject.BindEvent(OnClickTitleButton);
-        */
+        
 
 
         return true;
     }
 
+    public void OnClickResumeButton()
+    {
+        Managers.UI.ClosePopupUI(this);
+        Time.timeScale = 1f;
+    }
+    public void OnClickRetryButton()
+    {
+        Managers.UI.ClosePopupUI(this);
+        Time.timeScale = 1f;
+        Managers.Scene.LoadScene(Define.EScene.Test_Battle);
+
+    }
+    public void OnClickTitleButton()
+    {
+        Managers.UI.ClosePopupUI(this);
+        Time.timeScale = 1f; 
+        Managers.Scene.LoadScene(Define.EScene.GameScene);
+    }
 
 }
