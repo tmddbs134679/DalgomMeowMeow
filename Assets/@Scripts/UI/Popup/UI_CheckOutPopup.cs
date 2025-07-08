d4b75b1ff4d80f3f09e457af3c90d2940da1cc02
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-07-08 09:59:57 +0900

    feat : 1. UI 폰트 추가 2. UI_CheckOutPopup, UI_CheckOutItem 스크립트,UI 추가 및 구현중

--- Assets/@Scripts/UI/Popup/UI_CheckOutPopup.cs
+++ Assets/@Scripts/UI/Popup/UI_CheckOutPopup.cs
@@ -1,18 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Define;
 
-public class UI_CheckOutPopup : MonoBehaviour
+public class UI_CheckOutPopup : UI_Popup
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Enum
+    enum GameObjects
     {
-        
+        ContentObject,
+
     }
 
-    // Update is called once per frame
-    void Update()
+    enum Buttons
     {
-        
+        ExitButton,
+    }
+
+    enum Texts
+    {
+
+    }
+    #endregion
+
+
+    public int _CheckOutDay;
+    int _monthCount;
+    int _dailyCount;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void OnEnable()
+    {
+        PopupOpenAnimation(GetObject((int)GameObjects.ContentObject));
+    }
+
+
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        #region Object Bind
+        BindObject(typeof(GameObjects));
+        BindButton(typeof(Buttons));
+        BindText(typeof(Texts));
+
+        GetButton((int)Buttons.ExitButton).gameObject.BindEvent(OnClickExitButton);
+        GetButton((int)Buttons.ExitButton).gameObject.GetOrAddComponent<UI_ButtonAnimation>();
+
+        #endregion
+
+
+        Refresh();
+
+        return true;
+    }
+
+    public void SetInfo(int checkOutDay)
+    {
+
+    }
+
+    void Refresh()
+    {
+
+
+    }
+
+    private void OnClickExitButton()
+    {
+        Managers.UI.ClosePopupUI(this);
     }
 }
