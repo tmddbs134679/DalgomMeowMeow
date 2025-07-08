4bdb5a852a77b0df0a48e880e810439e932e8e6f
jinjuho2 <jinjuho3@gmail.com>
2025-07-08 19:42:32 +0900

    Feat: 승윤님 부탁드립니다

--- Assets/@Scripts/Managers/Contents/AIManager.cs
+++ Assets/@Scripts/Managers/Contents/AIManager.cs
@@ -1,33 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor.AddressableAssets.GUI;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.AI;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class AIManager : MonoBehaviour
+public class AIManager 
 {
     public static AIManager Instance;
 
     public List<AICharacter> AllCharacters  = new List<AICharacter>();
     public Material[] EmotionMaterials;
-    public void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
-    void Start()
+    public void Init()
     {
+        //await LoadEmotionMaterials();
+        
+       
         foreach (var character in AllCharacters)
         {
             ValidateNavMeshPosition(character);
         }
+
     }
 
     public void Register(AICharacter character)
@@ -58,6 +55,26 @@ public class AIManager : MonoBehaviour
         {
             Debug.LogWarning($"[{character.name}] NavMesh에서 이탈! 복구 시도");
             RelocateToNearestNavMesh(character);
+        }
+    }
+
+    private async Task LoadEmotionMaterials()
+    {
+        // Addressables 로드: 여러 개면 Label 또는 Asset Group으로 관리 권장
+        var handle = Addressables.LoadAssetsAsync<Material>(
+            "CharacterEmotion",  // Address 또는 Label
+            null);               // 개별 로드 콜백(없으면 null)
+
+        await handle.Task;
+
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            EmotionMaterials = handle.Result.ToArray();
+            Debug.Log($"[AIManager] CharacterEmotion 로드 완료: {EmotionMaterials.Length}개");
+        }
+        else
+        {
+            Debug.LogError("[AIManager] CharacterEmotion 로드 실패!");
         }
     }
 
--- Assets/@Scripts/Scenes/GameScene.cs
+++ Assets/@Scripts/Scenes/GameScene.cs
@@ -12,15 +12,19 @@ public class GameScene : BaseScene
 
         SceneType = Define.EScene.GameScene;
 
-        Managers.UI.ShowSceneUI<UI_GameScene>();
-
+        Managers.UI.ShowSceneUI<UI_GameScene>();    
         foreach (var ch in Managers.Game.Characters)
         {
             AICharacter ai = Managers.Object.Spawn<AICharacter>(ch.Pos.ToVector3(), ch.DataId);
+            ai.Init();
             ai.SetInfo(ch);
+            ai.emo = Managers.AI.EmotionMaterials;
+
             Managers.Game.CharactersInScene[ch.DataId] = ai;
             Managers.Game.SetInitEquipment(ai);
+            Managers.AI.Register(ai);
         }
+
     }
 
 
