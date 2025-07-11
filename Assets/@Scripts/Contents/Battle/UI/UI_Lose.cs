5e331f2df755d64ff1ca0d871a09553afeca134c
Sungju Kim <tjdwn0885@naver.com>
2025-07-11 14:26:02 +0900

    feat: 패배 팝업, 이미지 정리
    패배시 팝업 연결, 기타 이미지 정리

--- Assets/@Scripts/Contents/Battle/UI/UI_Lose.cs
+++ Assets/@Scripts/Contents/Battle/UI/UI_Lose.cs
@@ -1,59 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_PausePopup : UI_Popup   //어드레서블에 프리펩 넣기
+public class UI_Lose : UI_Popup
 {
     #region Enum
-
     enum Buttons
     {
-        ResumeButton,   //버튼이름(알아서 찾아옴, 게임오브젝트랑 이름 똑같이)
+        TitleButton,
         RetryButton,
-        TitleButton
     }
     #endregion
-
     private void Awake()
     {
         Init();
     }
 
-    private void OnEnable()
-    {
 
-    }
     public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
         BindButton(typeof(Buttons));
-        
-        GetButton((int)Buttons.ResumeButton).gameObject.BindEvent(OnClickResumeButton); //버튼 등록
+        GetButton((int)Buttons.TitleButton).gameObject.BindEvent(OnClickTitle);
         GetButton((int)Buttons.RetryButton).gameObject.BindEvent(OnClickRetryButton);
-        GetButton((int)Buttons.TitleButton).gameObject.BindEvent(OnClickTitleButton);
-        
-
 
         return true;
-    }
-
-    public void OnClickResumeButton()
-    {
-        Managers.UI.ClosePopupUI(this);
-        Time.timeScale = 1f;
     }
     public void OnClickRetryButton()
     {
         Managers.UI.ClosePopupUI(this);
         Time.timeScale = 1f;
         Managers.Scene.LoadScene(Define.EScene.Test_Battle);
-
     }
-    public void OnClickTitleButton()
+
+    public void OnClickTitle()
     {
         Managers.UI.ClosePopupUI(this);
-        Time.timeScale = 1f; 
+        Time.timeScale = 1f;
         Managers.Scene.LoadScene(Define.EScene.GameScene);
     }
-
 }
