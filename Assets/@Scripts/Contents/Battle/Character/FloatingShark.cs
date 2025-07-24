43944bb6834bcc20a2244e6d8d27e16a3c79582a
Sungju Kim <tjdwn0885@naver.com>
2025-07-24 13:37:32 +0900

    fix: 배틀 씬 꾸미기
    배틀씬 이뻐져요~

---
+++ Assets/@Scripts/Contents/Battle/Character/FloatingShark.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingShark : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke(nameof(Float), Random.Range(0f , 1f)); // 0.5초 후에 Float 메소드 호출
+    }
+
+    public void Float()
+    {
+        // 기준 위치
+        Vector3 startPos = new Vector3(transform.position.x, -0.5f, transform.position.z);
+
+        // 시작 위치 세팅
+        transform.position = startPos;
+        float offset = Random.Range(0.05f, 0.15f);
+
+        // 부유 애니메이션 (y축으로 반복 이동)
+        transform.DOMoveY(-0.5f + offset, 1.5f)
+                 .SetEase(Ease.InOutSine)
+                 .SetLoops(-1, LoopType.Yoyo)
+                 .SetUpdate(true); // 타임스케일 무시
+    }
+}
