9f4279cdfbbd041627854866dd01308af701f60c
kihyun <gosof@naver.com>
2025-07-29 15:42:41 +0900

    fix: questReward 수정
    .

--- Assets/@Scripts/Contents/Quest/Quest.cs
+++ Assets/@Scripts/Contents/Quest/Quest.cs
@@ -31,6 +31,7 @@ public class Quest
             State = QuestProgressState.Completed;
             Managers.Debug.Log($"[퀘스트 완료] {QuestData.Title}", Define.EDebugType.Building);
         }
+        
     }
 
     public void Reward()
@@ -39,7 +40,7 @@ public class Quest
         {
             State = QuestProgressState.Rewarded;
             Managers.Debug.Log($"[보상 지급] {QuestData.Reward}", Define.EDebugType.Building);
-            Managers.Game.Gold += QuestData.Reward;
+            Managers.Game.Dia += QuestData.Reward;
             
             // 다음 퀘스트 열기
             QuestManager.Instance.TryActivateNext(QuestData.QuestId);
