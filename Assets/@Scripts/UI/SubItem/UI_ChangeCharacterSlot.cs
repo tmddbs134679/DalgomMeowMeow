9f6c6af664727d09c9f908a7b1a419a92d8f72d6
jinjuho2 <jinjuho3@gmail.com>
2025-07-29 20:19:57 +0900

    Fix  : ForestPopupUI , ChangePopupUI scrollview 수정

--- Assets/@Scripts/UI/SubItem/UI_ChangeCharacterSlot.cs
+++ Assets/@Scripts/UI/SubItem/UI_ChangeCharacterSlot.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static Define;
 
@@ -46,6 +47,10 @@ public class UI_ChangeCharacterSlot : UI_Base
         BindImage(typeof(Images));
         BindText(typeof(Texts));
 
+        gameObject.BindEvent(null, OnDrag, Define.EUIEvent.Drag);
+        gameObject.BindEvent(null, OnBeginDrag, Define.EUIEvent.BeginDrag);
+        gameObject.BindEvent(null, OnEndDrag, Define.EUIEvent.EndDrag);
+
         gameObject.BindEvent(OnClickObjectButton);
         return true;
     }
@@ -60,6 +65,7 @@ public class UI_ChangeCharacterSlot : UI_Base
 
     Character _character;
     ScrollRect _scrollRect;
+    bool _isDrag = false;
 
     public void SetInfo(Character character, ScrollRect scrollRect)
     {
@@ -74,5 +80,26 @@ public class UI_ChangeCharacterSlot : UI_Base
         GetImage((int)Images.MoveSpeedIcon).sprite = Managers.Resource.Load<Sprite>("Bread");
 
         GetText((int)Texts.MoveSpeed).text = _character.MoveSpeed.ToString();
+    }
+
+    public void OnDrag(BaseEventData baseEventData)
+    {
+        _isDrag = true;
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        _scrollRect.OnDrag(pointerEventData);
+    }
+
+    public void OnBeginDrag(BaseEventData baseEventData)
+    {
+        _isDrag = true;
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        _scrollRect.OnBeginDrag(pointerEventData);
+    }
+
+    public void OnEndDrag(BaseEventData baseEventData)
+    {
+        _isDrag = false;
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        _scrollRect.OnEndDrag(pointerEventData);
     }
 }
