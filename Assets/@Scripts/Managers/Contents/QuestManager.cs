a078849d70644ca2d61592d25c08ad4f3dea79f3
kihyun <gosof@naver.com>
2025-07-31 18:10:12 +0900

    fix: dailyQuestupdate 수정
    .

--- Assets/@Scripts/Managers/Contents/QuestManager.cs
+++ Assets/@Scripts/Managers/Contents/QuestManager.cs
@@ -265,6 +265,7 @@ public class QuestManager : MonoBehaviour
     
     public void NotifyBuildingConstructed(string type)
     {
+        UpdateQuestProgress(Define.EQuestConditionType.Build, Define.ETargetType.Build);
         // string → ETargetType 변환
         if (!Enum.TryParse(type, out Define.ETargetType targetType))
         {
