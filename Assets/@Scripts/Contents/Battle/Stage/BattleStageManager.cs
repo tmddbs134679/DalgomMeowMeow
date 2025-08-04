1da511b44df14a64a2d407b48518b6af2a2c34d5
Sungju Kim <tjdwn0885@naver.com>
2025-08-04 14:54:04 +0900

    fix: 카메라 고정 삭제, UI 수정

--- Assets/@Scripts/Contents/Battle/Stage/BattleStageManager.cs
+++ Assets/@Scripts/Contents/Battle/Stage/BattleStageManager.cs
@@ -28,7 +28,6 @@ public class BattleStageManager : BaseObject
 
 
         RandomSet(); //적들 랜덤 생성
-        SetAspectRatio(); //화면 비율 설정
     }
 
 
@@ -67,37 +66,5 @@ public class BattleStageManager : BaseObject
     }
 
 
-    void SetAspectRatio()
-    {
-        float targetAspect = 20f / 9f;
-        float windowAspect = (float)Screen.width / (float)Screen.height;
-        float scaleHeight = windowAspect / targetAspect;
-
-        Camera camera = Camera.main;
-
-        if (scaleHeight < 1.0f)
-        {
-            Rect rect = camera.rect;
-
-            rect.width = 1.0f;
-            rect.height = scaleHeight;
-            rect.x = 0;
-            rect.y = (1.0f - scaleHeight) / 2.0f;
-
-            camera.rect = rect;
-        }
-        else
-        {
-            float scaleWidth = 1.0f / scaleHeight;
-
-            Rect rect = camera.rect;
-
-            rect.width = scaleWidth;
-            rect.height = 1.0f;
-            rect.x = (1.0f - scaleWidth) / 2.0f;
-            rect.y = 0;
-
-            camera.rect = rect;
-        }
-    }
+    
 }
